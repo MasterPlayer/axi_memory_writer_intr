095689a5744701f6516cfd57d3e0bc77cf2f66d7
Maxim <masterplayer@yandex.ru>
2024-04-07 15:19:59 +0300

    added .f file

---
+++ axi_memory_writer_intr.f
@@ -0,0 +1,11 @@
+# TOP
+    hw/axi_memory_writer_intr.sv
+
+# internal modules 
+    hw/axi_memory_writer_intr_functional.sv
+    hw/irq_generator.vhd
+    hw/irq_retryer.vhd
+
+# submodules
+    submodules/mpf_xpm_fifo_in_sync_counted/hw/mpf_xpm_fifo_in_sync_counted.sv
+    submodules/mpf_xpm_fifo_cmd_sync/hw/mpf_xpm_fifo_cmd_sync.sv
